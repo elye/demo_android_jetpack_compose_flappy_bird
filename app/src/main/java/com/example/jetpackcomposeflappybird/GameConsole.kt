9e3c9cad23fe0c2f017e49632914928c399dd758
Troy <troyhen@churchofjesuschrist.org>
2021-04-12 21:59:09 -0600

    Update to the latest compose, fix animations, and use normal composables rather than canvas

--- app/src/main/java/com/example/jetpackcomposeflappybird/GameConsole.kt
+++ app/src/main/java/com/example/jetpackcomposeflappybird/GameConsole.kt
@@ -1,93 +1,111 @@
 package com.example.jetpackcomposeflappybird
 
-import androidx.compose.animation.animatedFloat
-import androidx.compose.animation.core.AnimationConstants
+import android.view.MotionEvent
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.repeatable
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.res.loadImageResource
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 
 @Composable
 fun GameConsole(
     modifier: Modifier = Modifier,
-    gameState: MutableState<GameState>,
-    clickChange: MutableState<Boolean>,
-    gameScore: MutableState<Int>
-    ) {
+) {
+    val viewModel = viewModel<GameViewModel>()
+    viewModel.transition = rememberInfiniteTransition()
+    BoxWithConstraints(
+        modifier
+            .fillMaxSize()
+            .pointerInteropFilter {
+                var used = false
+                when (it.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        if (viewModel.isRunning) {
+                            viewModel.flap()
+                            used = true
+                        }
+                    }
+                }
+                used
+            }) {
+        viewModel.fieldRect = Rect(0f, 0f, maxWidth.value, maxHeight.value)
 
-    val bird = loadImageResource(id = R.drawable.flappy)
-    val background = loadImageResource(id = R.drawable.background)
-    val upPipe = loadImageResource(id = R.drawable.pipe_up)
-    val downPipe = loadImageResource(id = R.drawable.pipe_down)
-    val animatedFloat = animatedFloat(initVal = 0f)
-    val birdState = remember(bird) { BirdState(bird) }
-    val backgroundState = remember(background) { BackgroundState(background) }
-    val pipesState = remember(upPipe, downPipe) { PipesState(upPipe, downPipe, gameScore) }
-
-    remember(clickChange.value) { birdState.jump() }
-
-    Canvas(modifier = modifier) {
-        val gameRect = Rect(0f, 0f, size.width, size.height)
-
-        if (animatedFloat.isRunning && gameState.value.isFinished()) {
-            animatedFloat.stop()
-        } else if (!animatedFloat.isRunning && gameState.value.isRunning()) {
-            animatedFloat.snapTo(0f)
-            animatedFloat.animateTo(
-                targetValue = 1f,
-                anim = repeatable(
-                    iterations = AnimationConstants.Infinite,
-                    animation = tween(durationMillis = 250, easing = LinearEasing),
-                )
-            )
-            pipesState.init(this)
-            birdState.init()
-            gameScore.value = 0
-        }
-        val tick = animatedFloat.value
-
-        if (gameState.value.isRunning()) {
-            backgroundState.move()
-            birdState.move()
-            pipesState.move()
-        } else if (gameState.value.isEnding()){
-            birdState.dying()
-        }
-
-        backgroundState.draw(this, tick)
-        val pipeRects = pipesState.draw(this, tick)
-        val birdRect = birdState.draw(this, tick, gameState.value)
-
-        if (gameState.value.isRunning()) {
-            checkCollision(gameState, gameRect, pipeRects, birdRect)
-        } else if (gameState.value.isEnding()) {
-            finishFalling(gameState, gameRect, birdRect)
+        Background(viewModel)
+        Pipes(viewModel)
+        Bird(viewModel)
+        Score(viewModel.gameScore, Modifier.fillMaxWidth())
+        if (viewModel.isStopped) {
+            Start(Modifier.fillMaxSize()) {
+                viewModel.start()
+            }
         }
     }
 }
 
-private fun finishFalling(
-    animationStart: MutableState<GameState>,
-    gameRect: Rect,
-    birdRect: Rect) {
-    if (!gameRect.overlaps(birdRect)) {
-        animationStart.value = GameState(GameState.Status.STOPPED)
+@Composable
+private fun Score(gameScore: Int, modifier: Modifier = Modifier) {
+    Text(
+        modifier = modifier,
+        fontSize = 48.sp,
+        text = gameScore.toString(),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun Start(
+    modifier: Modifier = Modifier,
+    onStart: () -> Unit
+) {
+    TextButton(modifier = modifier, onClick = {
+        onStart()
+    }) {
+        Text(
+            text = "\n\n\nClick to Start",
+            fontSize = 32.sp
+        )
     }
 }
 
-private fun checkCollision(
-    animationStart: MutableState<GameState>,
-    gameRect: Rect,
-    pipeRects: List<Rect>,
-    birdRect: Rect) {
-    if (!gameRect.overlaps(birdRect) || pipeRects.firstOrNull { it.overlaps(birdRect) } != null) {
-        animationStart.value = GameState(GameState.Status.ENDING)
+@Composable
+fun Background(viewModel: GameViewModel = viewModel()) {
+    val fieldRect = viewModel.fieldRect ?: return
+    val image = painterResource(R.drawable.background)
+    var fraction by remember { mutableStateOf(0f) }
+    if (viewModel.isRunning) {
+        val animation = viewModel.transition.animateFloat(
+            initialValue = 0f,
+            targetValue = 1f,
+            animationSpec = infiniteRepeatable(animation = tween(durationMillis = 10000, easing = LinearEasing))
+        )
+        fraction = animation.value
     }
+    val width = fieldRect.width.dp
+    val currentPosX = -width * fraction
+    val modifier = Modifier.size(width, fieldRect.height.dp)
+    Image(painter = image, contentDescription = null, modifier = modifier.offset(x = currentPosX), contentScale = ContentScale.Crop)
+    Image(painter = image, contentDescription = null, modifier = modifier.offset(x = currentPosX + width), contentScale = ContentScale.Crop)
 }
